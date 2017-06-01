4f0e4c9c9fa97efc4ae4d1acc45e9516d1895c6e
Sergii Borysenko <s.borysenko@rickerlyman.com>
2017-06-02 01:53:24 +0300

    Implementation of the Consumer with Rebalance listener. Listener used for rebalance, change offsets only.

--- src/main/scala/test/kafka/partitioning/Consumer.scala
+++ src/main/scala/test/kafka/partitioning/Consumer.scala
@@ -8,62 +8,62 @@ import scala.collection.JavaConversions._
 import org.apache.kafka.clients.consumer.{ConsumerRebalanceListener, ConsumerRecords, KafkaConsumer}
 import org.apache.kafka.common.TopicPartition
 
+import scala.collection.mutable
+
 /**
   * Created by sergey on 5/24/17.
   */
-class Consumer(name: String, topic: String) extends Runnable {
-  var consumer: KafkaConsumer[String, String] = _
-  val props = new Properties()
+class Consumer(name: String, topic: String) {
+  def consume: Unit = {
+    val consumer = getConsumer()
+    val lastOffsets = mutable.HashMap.empty[Int, Long]
 
-  def init(): Consumer = {
-    println("Read initial state for consumer " + name)
+    consumer.subscribe(util.Collections.singletonList(topic), new ConsumerRebalanceListener {
+      override def onPartitionsAssigned(partitions: util.Collection[TopicPartition]): Unit = {
+        println("consumer: " + name + ": onPartitionsAssigned with: " + partitions)
 
-    consumer = getConsumer()
-    consumer.subscribe(util.Collections.singletonList("test"))
-    consumer.endOffsets(consumer.assignment())
+        partitions.asScala.foreach(partition => {
+          lastOffsets += (partition.partition() -> (consumer.position(partition)))
+          println("consumer: " + name + ", topic: " + partition.topic() + ", partition: " + partition.partition() + ", offset:" +  consumer.position(partition))
+        })
 
+        consumer.seekToBeginning(partitions)
+      }
 
-    consumer.seekToBeginning(consumer.assignment())
+      override def onPartitionsRevoked(partitions: util.Collection[TopicPartition]): Unit = {
+        println("consumer: " + name + ": onPartitionsRevoked with: " + partitions)
+      }
+    })
 
-    _read("I")
+    println("consumer: " + name + " started listening")
+    while(true) {
+        val inputRecords = consumer.poll(1000)
+        for (record <- inputRecords) {
+          var action = "store"
+          if (record.offset() >= lastOffsets(record.partition())) action = "process"
+          println("consumer: " + name + ", partition: " + record.partition() + ", offset: " + record.offset() + ", value: " + record.value() + " action: " + action)
+        }
 
-    consumer.unsubscribe()
-
-    this
+    }
   }
 
   def getConsumer(): KafkaConsumer[String, String] ={
+    val props = new Properties()
+
     props.put("bootstrap.servers", "localhost:9092")
     props.put("group.id", "test-group")
     props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
     props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-    props.put("enable.auto.commit", "false")
+    props.put("enable.auto.commit", "true")
     props.put("auto.commit.interval.ms", "1000")
     props.put("session.timeout.ms", "30000")
 
     new KafkaConsumer[String, String](props)
   }
+}
 
-  def run(): Unit = {
-    println("Starting consumer " + name)
-
-//    consumer = getConsumer()
-//    consumer.subscribe(util.Collections.singletonList("test"));
-
-    try {
-      while (true) {
-        _read("L")
-      }
-    } finally {
-      consumer.close()
-    }
-  }
-
-  def _read(mark: String): Unit = {
-    val inputRecords = consumer.poll(1000)
-    val iterator = inputRecords.iterator()
-    for (record <- inputRecords) {
-      println(mark + "consumer " + name + ": " + record.partition() + ": " + record.offset() + ": " + record.value())
-    }
+object Consumer {
+  def main(args: Array[String]): Unit = {
+    new Consumer("0", "test-1").consume
   }
 }
